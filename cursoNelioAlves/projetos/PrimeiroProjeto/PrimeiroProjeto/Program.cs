066150e1f36c40fc099984bdd3f35f16ecda235f
JP Ferreira <jotepauder01@gmail.com>
2025-12-10 21:17:41 -0300

    Primeiro projeto do curso

---
+++ cursoNelioAlves/projetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args) // enterPoint da aplicação do C#
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Finalmente porra");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
